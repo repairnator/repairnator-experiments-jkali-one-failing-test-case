0849fb72a46ec547965ec300f04ad9ad84da3d92
repairnator <noreply@github.com>
2020-02-15 14:50:06 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-375808753-20180507-113507-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/DefaultLineAlternation.java
@@ -0,0 +1,57 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.MixException;
+import innovimax.mixthem.arguments.ParamValue;
+import innovimax.mixthem.arguments.RuleParam;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+* <p>Alternates two lines.</p>
+* @see ILineOperation
+* @author Innovimax
+* @version 1.0
+*/
+public class DefaultLineAlternation extends AbstractLineOperation {
+
+	private final AltMode mode;
+	private boolean odd;
+	private final Random random;
+
+	/**
+	* @param mode The alternate mode to process
+ 	* @param params The list of parameters (maybe empty)	
+	* @see innovimax.mixthem.operation.RuleParam
+	* @see innovimax.mixthem.operation.ParamValue
+	*/
+	public DefaultLineAlternation(final AltMode mode, final Map<RuleParam, ParamValue> params) {
+		super(params);
+		this.mode = mode;
+		this.odd = true;
+		this.random = new Random(params.getOrDefault(RuleParam.RANDOM_SEED, AltOperation.DEFAULT_RANDOM_SEED.getValue()).asInt());
+	}	
+
+	@Override
+	public void process(final String line1, final String line2, final LineResult result) throws MixException {		
+		result.reset();
+		if (line1 == null) {
+			result.setResult(line2);
+		} else if (line2 == null) {
+			result.setResult(line1);
+		} else {
+			String line = null;
+			switch (this.mode) {
+				case RANDOM:
+					line = this.random.nextBoolean() ? line1 : line2;
+					break;
+				case NORMAL:
+				default:
+					line = this.odd ? line1 : line2;
+					this.odd = !this.odd;
+			}
+			result.setResult(line);
+		}
+	}
+
+}
