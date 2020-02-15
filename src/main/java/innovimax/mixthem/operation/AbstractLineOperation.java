265962db215a6d69fa2b317818ac5a4276d42c91
repairnator <noreply@github.com>
2020-02-15 14:47:50 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-365991811-20180413-100635-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/AbstractLineOperation.java
@@ -0,0 +1,53 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.MixException;
+import innovimax.mixthem.arguments.RuleParam;
+import innovimax.mixthem.arguments.ParamValue;
+import innovimax.mixthem.io.DefaultLineReader;
+import innovimax.mixthem.io.DefaultLineWriter;
+import innovimax.mixthem.io.IInputLine;
+import innovimax.mixthem.io.IOutputLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+* <p>Abstract class for all line operation.</p>
+* @see ILineOperation
+* @author Innovimax
+* @version 1.0
+*/
+public abstract class AbstractLineOperation extends AbstractOperation implements ILineOperation {
+	
+	/**
+	* Constructor
+ 	* @param params The list of parameters (maybe empty)
+	* @see innovimax.mixthem.arguments.RuleParam
+	* @see innovimax.mixthem.arguments.ParamValue
+	*/
+	public AbstractLineOperation(Map<RuleParam, ParamValue> params) {
+		super(params);
+	}
+
+	@Override
+	public void processFiles(File file1, File file2, OutputStream out) throws MixException, IOException {
+		IInputLine reader1 = new DefaultLineReader(file1);
+		IInputLine reader2 = new DefaultLineReader(file2);
+		IOutputLine writer = new DefaultLineWriter(out);
+		LineResult result = new LineResult();
+		while (reader1.hasLine() || reader2.hasLine()) {
+			final String line1 = result.firstFileReading() ? reader1.nextLine() : result.getFirstLine();
+			final String line2 = result.secondFileReading() ? reader2.nextLine() : result.getSecondLine();
+			process(line1, line2, result);
+			if (result.hasResult()) {
+				writer.writeLine(result.getResult());
+			}
+        	}
+        	reader1.close();
+        	reader2.close();
+        	writer.close();				
+    	}
+
+}
---
+++ src/main/java/innovimax/mixthem/operation/DefaultLineAlternation.java
@@ -0,0 +1,62 @@
+package innovimax.mixthem.operation;
+
+import static innovimax.mixthem.MixConstants.*;
+import innovimax.mixthem.MixException;
+import innovimax.mixthem.arguments.ParamValue;
+import innovimax.mixthem.arguments.RuleParam;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+* <p>Alternates two lines.</p>
+* @see ILineOperation
+* @author Innovimax
+* @version 1.0
+*/
+public class DefaultLineAlternation extends AbstractLineOperation {
+
+	private final AltMode mode;
+	private boolean odd;
+	private final Random random;
+
+	/**
+	* @param mode The alternate mode to process
+ 	* @param params The list of parameters (maybe empty)	
+	* @see innovimax.mixthem.operation.RuleParam
+	* @see innovimax.mixthem.operation.ParamValue
+	*/
+	public DefaultLineAlternation(AltMode mode, Map<RuleParam, ParamValue> params) {
+		super(params);
+		this.mode = mode;
+		this.odd = true;
+		this.random = new Random(DEFAULT_RANDOM_SEED);
+		if (this.params.containsKey(RuleParam._RANDOM_SEED)) {
+			this.random.setSeed(this.params.get(RuleParam._RANDOM_SEED).asInt());
+		}
+	}	
+
+	@Override
+	public void process(String line1, String line2, LineResult result) throws MixException {		
+		result.resetTypes();
+		if (line1 == null) {
+			result.setResult(line2);
+		} else if (line2 == null) {
+			result.setResult(line1);
+		} else {
+			String line = null;
+			switch (this.mode) {
+				case _RANDOM:
+					line = this.random.nextBoolean() ? line1 : line2;
+					break;
+				case _NORMAL:
+				default:
+					line = this.odd ? line1 : line2;
+					this.odd = !this.odd;
+			}
+			result.setResult(line);
+		}
+		result.exploreBothFiles();
+	}
+
+}
