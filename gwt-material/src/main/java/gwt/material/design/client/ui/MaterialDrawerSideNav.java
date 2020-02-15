573691429032502038546edf10310144e3d25ba6
repairnator <noreply@github.com>
2020-02-15 14:49:57 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-GwtMaterialDesign-gwt-material-219537671-20170407-052517-firstCommit

---
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialDrawerSideNav.java
@@ -0,0 +1,84 @@
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 - 2018 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package gwt.material.design.client.ui;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import gwt.material.design.client.base.HasWithHeader;
+import gwt.material.design.client.constants.SideNavType;
+
+import static gwt.material.design.client.js.JsMaterialElement.$;
+
+public class MaterialDrawerSideNav extends MaterialSideNav implements HasWithHeader {
+
+    private HandlerRegistration overlayOpeningHandler;
+
+    public MaterialDrawerSideNav() {
+        super(SideNavType.DRAWER);
+    }
+
+    @Override
+    protected void build() {
+        setWithHeader(false);
+    }
+
+    @Override
+    public void setWithHeader(boolean withHeader) {
+        if (withHeader) {
+            applyOverlayWithHeader();
+        } else {
+            applyOverlayType();
+        }
+    }
+
+    /**
+     * Provides an overlay sidenav just like when opening sidenav on mobile / tablet
+     */
+    protected void applyOverlayType() {
+        setType(SideNavType.DRAWER);
+        setShowOnAttach(false);
+        if (overlayOpeningHandler == null) {
+            overlayOpeningHandler = addOpeningHandler(event -> {
+                Scheduler.get().scheduleDeferred(() -> $("#sidenav-overlay").css("visibility", "visible"));
+            });
+        }
+        Scheduler.get().scheduleDeferred(() -> {
+            pushElement(getHeader(), 0);
+            pushElement(getMain(), 0);
+        });
+    }
+
+    /**
+     * Provides an overlay sidenav that will float on top of the content not the navbar without
+     * any grey overlay behind it.
+     */
+    protected void applyOverlayWithHeader() {
+        setType(SideNavType.DRAWER_WITH_HEADER);
+        setShowOnAttach(false);
+        applyTransition(getMain(), 200);
+        applyBodyScroll();
+        if (isShowOnAttach()) {
+            Scheduler.get().scheduleDeferred(() -> {
+                pushElement(getHeader(), 0);
+                pushElement(getMain(), 0);
+            });
+        }
+    }
+}
---
+++ gwt-material/src/main/java/gwt/material/design/client/ui/MaterialPushSideNav.java
@@ -0,0 +1,107 @@
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 - 2018 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package gwt.material.design.client.ui;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import gwt.material.design.client.base.HasWithHeader;
+import gwt.material.design.client.constants.SideNavType;
+import gwt.material.design.jquery.client.api.JQuery;
+
+import static gwt.material.design.client.js.JsMaterialElement.$;
+
+public class MaterialPushSideNav extends MaterialSideNav implements HasWithHeader {
+
+    private HandlerRegistration pushWithHeaderOpeningHandler;
+    private HandlerRegistration pushWithHeaderClosingHandler;
+
+    public MaterialPushSideNav() {
+        super(SideNavType.PUSH);
+    }
+
+    @Override
+    protected void build() {
+        setWithHeader(false);
+    }
+
+    @Override
+    public void setWithHeader(boolean withHeader) {
+        if (withHeader) {
+            applyPushWithHeader();
+        } else {
+            applyPushType();
+        }
+    }
+
+    /**
+     * Push the header, footer, and main to the right part when Close type is applied.
+     */
+    protected void applyPushType() {
+        setType(SideNavType.PUSH);
+        $(JQuery.window()).off("resize").resize((e, param1) -> {
+            if (!isAlwaysShowActivator() && !isOpen() && gwt.material.design.client.js.Window.matchMedia("all and (min-width: 992px)")) {
+                show();
+            }
+            pushElements(isOpen(), getWidth());
+            return true;
+        });
+    }
+
+    protected void applyPushWithHeader() {
+        MaterialToast.fireToast("WITH HEADER");
+        setType(SideNavType.PUSH_WITH_HEADER);
+        applyTransition(getMain(), 200);
+        applyTransition(getFooter(), 200);
+        applyBodyScroll();
+        if (isShowOnAttach()) {
+            Scheduler.get().scheduleDeferred(() -> {
+                pushElement(getHeader(), 0);
+                pushElement(getMain(), getWidth());
+                pushElementMargin(getFooter(), getWidth());
+            });
+        }
+
+        if (pushWithHeaderOpeningHandler == null) {
+            pushWithHeaderOpeningHandler = addOpeningHandler(event -> {
+                pushElement(getMain(), getWidth());
+                pushElementMargin(getFooter(), getWidth());
+            });
+        }
+
+        if (pushWithHeaderClosingHandler == null) {
+            pushWithHeaderClosingHandler = addClosingHandler(event -> {
+                pushElement(getMain(), 0);
+                pushElementMargin(getFooter(), 0);
+            });
+        }
+    }
+
+    @Override
+    protected void onClosing() {
+        super.onClosing();
+        pushElements(false, getWidth());
+    }
+
+    @Override
+    protected void onOpening() {
+        super.onOpening();
+        pushElements(true, getWidth());
+    }
+}
