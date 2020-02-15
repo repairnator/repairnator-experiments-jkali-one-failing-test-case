719af2d601eb293d0e99ec1480fa2bfbc4a692d6
repairnator <noreply@github.com>
2020-02-15 14:48:43 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-366101926-20180413-151228-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/ResultType.java
@@ -0,0 +1,20 @@
+package innovimax.mixthem.operation;
+
+/**
+* <p>This is a enumeration of result type.</p>
+* <p>Here are the types:</p>
+* <ul>
+* <li>HAS_RESULT: has a result</li>
+* <li>READ_FIRST_FILE: read first file</li>
+* <li>READ_SECOND_FILE: read second file</li>
+* <li>READ_BOTH_FILES: read both files</li>
+* </ul>
+* @author Innovimax
+* @version 1.0
+*/
+public enum ResultType {
+    HAS_RESULT,
+    READ_FIRST_FILE,
+    READ_SECOND_FILE,
+    READ_BOTH_FILES;
+}
