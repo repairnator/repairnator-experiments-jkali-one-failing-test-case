599b49d6e357e0518ecef00b518b1c866ef378d7
repairnator <noreply@github.com>
2020-02-15 23:14:09 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-ashtanyuk-TestCalcMvn-357993375-20180325-101140-firstCommit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by anton on 24.01.18.
+ */
+public class Calculator {
+    public static int add(int x,int y) {
+        return x+y;
+        
+    }
+    public static int sub(int x,int y) {
+        return x-y;
+    }
+    public static int mul(int x,int y) {
+        return x*y;
+    }
+    public static int div(int x,int y) {
+        return x*y;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by anton on 24.01.18.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
