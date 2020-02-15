5e7c6105e2005da0a742e3a9114f2e962be952df
repairnator <noreply@github.com>
2020-02-15 14:48:58 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-369976137-20180423-094415-firstCommit

---
+++ src/main/java/innovimax/mixthem/arguments/ArgumentException.java
@@ -0,0 +1,14 @@
+package innovimax.mixthem.arguments;
+
+/**
+* Signals any exception that occur during arguments checking.
+* @author Innovimax
+* @version 1.0
+*/
+public class ArgumentException extends Exception {
+
+	public ArgumentException(String message) { 
+		super(message); 
+	}
+	
+}
