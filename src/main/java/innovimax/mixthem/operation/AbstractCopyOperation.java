798a02d5f5aa3d4730519fb69030563c473ce98f
repairnator <noreply@github.com>
2020-02-15 18:17:09 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-385708486-20180530-160917-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/AbstractCopyOperation.java
@@ -0,0 +1,53 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.utils.StreamUtils;
+
+import innovimax.mixthem.MixException;
+import innovimax.mixthem.arguments.RuleParam;
+import innovimax.mixthem.arguments.ParamValue;
+import innovimax.mixthem.io.InputResource;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+/**
+* <p>Abstract class for all character operation.</p>
+* @see ICopyOperation
+* @author Innovimax
+* @version 1.0
+*/
+public abstract class AbstractCopyOperation extends AbstractOperation implements ICopyOperation {
+	
+	protected final static int BUFFER_SIZE = 1024;	
+
+	/**
+	* Constructor
+	* @param selection The file index selection (maybe empty)
+ 	* @param params The list of parameters (maybe empty)
+	* @see innovimax.mixthem.arguments.RuleParam
+	* @see innovimax.mixthem.arguments.ParamValue
+	*/
+	public AbstractCopyOperation(final Set<Integer> selection, final Map<RuleParam, ParamValue> params) {
+		super(selection, params);
+	}
+
+	@Override
+	public void processFiles(final List<InputResource> inputs, final OutputStream output) throws MixException, IOException {		
+		final IntStream indexes; 		
+		if (this.selection.isEmpty()) {
+			indexes = IntStream.range(1, inputs.size()+1);			
+		} else {			
+			indexes = Arrays.stream(this.selection.stream()
+					.mapToInt(Number::intValue)
+					.toArray());
+		}				
+                indexes.mapToObj(i -> inputs.get(i-1))
+                	.forEach(StreamUtils.consume(input -> process(input, output)));
+	}
+
+}
