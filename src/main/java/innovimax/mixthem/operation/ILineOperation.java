bb5c1ecc3fb052a48c77310b02a7c8751bb768b6
repairnator <noreply@github.com>
2020-02-15 14:49:38 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-371609026-20180426-171217-firstCommit

---
+++ src/main/java/innovimax/mixthem/operation/ILineOperation.java
@@ -0,0 +1,16 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.MixException;
+
+interface ILineOperation extends IOperation {
+	/**
+ 	* Processes operation and set new result in the LineResult parameter.
+	* @param line1 The first line to join (maybe null)
+ 	* @param line2 The second line to join (maybe null)
+	* @param result The previous operation result
+ 	* @return The result of the operation (maybe null)
+ 	* @throws MixException - If an mixing error occurs
+	* @see innovimax.mixthem.operation.LineResult
+ 	*/
+	void process(String line1, String line2, LineResult result) throws MixException;
+}
