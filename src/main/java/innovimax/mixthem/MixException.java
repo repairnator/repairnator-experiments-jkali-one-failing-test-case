65aefe0d9dc5c198f3b9d3ac510439ceada12100
repairnator <noreply@github.com>
2020-02-15 14:49:37 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-Innovimax-SARL-mix-them-371583543-20180426-161951-firstCommit

---
+++ src/main/java/innovimax/mixthem/MixException.java
@@ -0,0 +1,14 @@
+package innovimax.mixthem;
+
+/**
+* Signals any exception that occur during mixing.
+* @author Innovimax
+* @version 1.0
+*/
+public class MixException extends Exception {
+
+	public MixException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
