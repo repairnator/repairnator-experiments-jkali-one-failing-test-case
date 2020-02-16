71d853d6ca67e60a41e7ec091c0b649c29ee296f
repairnator <noreply@github.com>
2020-02-16 11:42:24 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-dropwizard-metrics-316104800-20171213-223115-firstCommit

---
+++ metrics-core/src/test/java/com/codahale/metrics/ClockTest.java
@@ -0,0 +1,28 @@
+package com.codahale.metrics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class ClockTest {
+
+    @Test
+    public void userTimeClock() throws Exception {
+        final Clock.UserTimeClock clock = new Clock.UserTimeClock();
+
+        assertThat((double) clock.getTime())
+                .isEqualTo(System.currentTimeMillis(),
+                        offset(100.0));
+
+        assertThat((double) clock.getTick())
+                .isEqualTo(System.nanoTime(),
+                        offset(100000.0));
+    }
+
+    @Test
+    public void defaultsToUserTime() throws Exception {
+        assertThat(Clock.defaultClock())
+                .isInstanceOf(Clock.UserTimeClock.class);
+    }
+}
