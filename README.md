9627a8756c16b7ac40a2ad2b417a14d288a191f7
repairnator <noreply@github.com>
2020-02-16 18:28:07 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-sbaudoin-yamllint-390493735-20180610-221803-firstCommit

---
+++ README.md
@@ -0,0 +1,19 @@
+YAML Lint
+=========
+
+YAML lint written in Java.
+It's main purpose is to provide an API and scripts to analyze YAML documents.
+
+[![Build Status](https://travis-ci.org/sbaudoin/yamllint.svg?branch=master)](https://travis-ci.org/sbaudoin/yamllint)
+
+## API usage
+See JavaDoc
+
+## Batch tool
+Get the `.zip` or `.tar.gz` distribution archive, unpack and execute the script corresponding to your platform in the `bin` directory:
+
+    bin/yamllint ...
+
+or
+
+    bin\yamllint ...
---
+++ src/test/java/com/github/sbaudoin/yamllint/rules/RuleFactoryTest.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2018, Sylvain Baudoin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.sbaudoin.yamllint.rules;
+
+import junit.framework.TestCase;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+import java.util.List;
+import java.util.logging.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RuleFactoryTest extends TestCase {
+    public void testGetRule() {
+        // Temporarily remove console handler
+        Logger logger = Logger.getLogger(RuleFactory.class.getName()).getParent();
+        ConsoleHandler ch = (ConsoleHandler)logger.getHandlers()[0];
+        logger.removeHandler(ch);
+
+        // Add memory handler to check logs
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        StreamHandler sh = new StreamHandler(bos, new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                return logRecord.getMessage() + System.lineSeparator();
+            }
+        });
+        sh.setLevel(Level.WARNING);
+        logger.addHandler(sh);
+
+        // All known rules
+        assertNotNull(RuleFactory.instance.getRule("braces"));
+        assertNotNull(RuleFactory.instance.getRule("brackets"));
+        assertNotNull(RuleFactory.instance.getRule("colons"));
+        assertNotNull(RuleFactory.instance.getRule("commas"));
+        assertNotNull(RuleFactory.instance.getRule("comments"));
+        assertNotNull(RuleFactory.instance.getRule("comments-indentation"));
+        assertNotNull(RuleFactory.instance.getRule("document-end"));
+        assertNotNull(RuleFactory.instance.getRule("document-start"));
+        assertNotNull(RuleFactory.instance.getRule("empty-lines"));
+        assertNotNull(RuleFactory.instance.getRule("empty-values"));
+        assertNotNull(RuleFactory.instance.getRule("hyphens"));
+        assertNotNull(RuleFactory.instance.getRule("indentation"));
+        assertNotNull(RuleFactory.instance.getRule("key-duplicates"));
+        assertNotNull(RuleFactory.instance.getRule("key-ordering"));
+        assertNotNull(RuleFactory.instance.getRule("line-length"));
+        assertNotNull(RuleFactory.instance.getRule("new-line-at-end-of-file"));
+        assertNotNull(RuleFactory.instance.getRule("new-lines"));
+        assertNotNull(RuleFactory.instance.getRule("octal-values"));
+        assertNotNull(RuleFactory.instance.getRule("trailing-spaces"));
+        assertNotNull(RuleFactory.instance.getRule("truthy"));
+
+        // Unknown rule
+        assertNull(RuleFactory.instance.getRule("this-rule-does-not-exist"));
+
+        // Check broken rule warnings
+        sh.flush();
+        List<String> lines = new BufferedReader(new StringReader(bos.toString())).lines().collect(Collectors.toList());
+        assertEquals(3, lines.size());
+        lines.forEach(line -> assertEquals("Cannot instantiate rule class com.github.sbaudoin.yamllint.rules.BrokenRule, will ignore it", line));
+
+        // Set back console handler
+        logger.removeHandler(sh);
+        sh.close();
+        logger.addHandler(ch);
+    }
+}
