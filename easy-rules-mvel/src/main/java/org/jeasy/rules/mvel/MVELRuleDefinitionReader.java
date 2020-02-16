98ccd5fa053293a84c9984dd0060d726a3869068
repairnator <noreply@github.com>
2020-02-16 11:38:53 +0100

    Bug commit from repairnator-repairnator-experiments This commit is based on the source code from the following commit: repairnator-repairnator-experiments-j-easy-easy-rules-360669764-20180331-193746-firstCommit

---
+++ easy-rules-mvel/src/main/java/org/jeasy/rules/mvel/MVELRuleDefinitionReader.java
@@ -0,0 +1,82 @@
+/**
+ * The MIT License
+ *
+ *  Copyright (c) 2017, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.jeasy.rules.mvel;
+
+import org.jeasy.rules.api.Rule;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("unchecked")
+class MVELRuleDefinitionReader {
+
+    private Yaml yaml = new Yaml();
+
+    MVELRuleDefinition read(File descriptor) throws FileNotFoundException {
+        return read(new FileReader(descriptor));
+    }
+
+    MVELRuleDefinition read(String descriptor) {
+        return read(new StringReader(descriptor));
+    }
+
+    private MVELRuleDefinition read(Reader reader) {
+        Object object = yaml.load(reader);
+        Map<String, Object> map = (Map<String, Object>) object;
+        return createRuleDefinitionFrom(map);
+    }
+
+    private static MVELRuleDefinition createRuleDefinitionFrom(Map<String, Object> map) {
+        MVELRuleDefinition ruleDefinition = new MVELRuleDefinition();
+
+        String name = (String) map.get("name");
+        ruleDefinition.setName(name != null ? name : Rule.DEFAULT_NAME);
+
+        String description = (String) map.get("description");
+        ruleDefinition.setDescription(description != null ? description : Rule.DEFAULT_DESCRIPTION);
+
+        Integer priority = (Integer) map.get("priority");
+        ruleDefinition.setPriority(priority != null ? priority : Rule.DEFAULT_PRIORITY);
+
+        String condition = (String) map.get("condition");
+        if (condition == null ) {
+            throw new IllegalArgumentException("The rule condition must be specified");
+        }
+        ruleDefinition.setCondition(condition);
+
+        List<String> actions = (List<String>) map.get("actions");
+        if (actions == null || actions.isEmpty()) {
+            throw new IllegalArgumentException("The rule action(s) must be specified");
+        }
+        ruleDefinition.setActions(actions);
+
+        return ruleDefinition;
+    }
+}
